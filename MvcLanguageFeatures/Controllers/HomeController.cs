99a72b53b31a8b9e768d95e9f3e909bada7d2230
knolegsb <seon2you@hotmail.com>
2015-10-27 10:44:33 -0700

    controller result sent to view

--- MvcLanguageFeatures/Controllers/HomeController.cs
+++ MvcLanguageFeatures/Controllers/HomeController.cs
@@ -23,6 +23,30 @@ namespace MvcLanguageFeatures.Controllers
             return View("Result", (object)String.Format("Product name: {0}", productName));
         }
 
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                { "apple", 10 }, { "orange", 20 }, { "plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product();
+
+            myProduct.ProductID = 100;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "A boat for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersports";
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
         //public ActionResult About()
         //{
         //    ViewBag.Message = "Your application description page.";
--- MvcLanguageFeatures/Models/Product.cs
+++ MvcLanguageFeatures/Models/Product.cs
@@ -7,15 +7,36 @@ namespace MvcLanguageFeatures.Models
 {
     public class Product
     {
+        private int productID;
         private string name;
-        public int ProductID { get; set; }
+        private string description;
+        private decimal price;
+        private string category;
+                
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
         public string Name
         {
-            get { return ProductID + " " + name; }
+            get { return name; }
             set { name = value; }
         }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string Category { get; set; }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
     }
 }
